e8abf7d3d38a5181bc18b2ce5243644ba91c71bf
haoshangke <1002995948@qq.com>
2019-08-02 09:26:02 +0800

    Initial commit

---
+++ JavaWeb/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: hsk
+  Date: 2019/8/1
+  Time: 10:20
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>尚硅谷学院</title>
+  </head>
+  <body>
+  <h1 style="color: red">Hello,IDEA</h1>
+  </body>
+</html>
---
+++ Module01/src/com/atguigu/bean/Customer.java
@@ -0,0 +1,33 @@
+package com.atguigu.bean;
+
+public class Customer {
+//ni
+
+//sy
+
+    /**
+     * @param args
+     *
+     */
+
+    public static final int num=1;
+    
+    public static void main (String[] args){
+        System.out.println("Hello World!!");
+
+    }
+    public void testUdate(){
+        //修改的测试
+    }
+
+    /**
+     * 客户的id
+     */
+    private int id = 1;
+    /**
+     *客户的姓名
+     */
+     private String name;
+     
+
+}
